bc1ed3b06f856aefa08a0f26e6cb1a3d4b91472f
KeiT <ndkhanh.2404@gmail.com>
2020-11-26 09:30:47 +0700

    update

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   databaseURL:
     "mongodb+srv://keitcoder:JgvNACekTnhPK2Ic@cluster0.3rjvx.mongodb.net/giphy-app?retryWrites=true&w=majority",
-  secretOrKey: "secret",
+  secretOrKey: "KeiTCoder",
 };
